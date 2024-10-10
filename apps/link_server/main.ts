932977bdba28f0db70c8858c65e0b0604be2975b
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 23:39:14 +0200

    TRY different urls

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -8,14 +8,19 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-  res.redirect(
-    307,
-    `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
-  );
+  res.redirect(`/test?code=${code}`);
 });
 
 app.get('/aztro-link/token', (req, res) => {
   console.log('req.query', req.query);
+});
+
+app.post('/test', (req, res) => {
+  const { code } = req.query;
+  res.redirect(
+    307,
+    `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
+  );
 });
 
 app.listen(port, () => {
