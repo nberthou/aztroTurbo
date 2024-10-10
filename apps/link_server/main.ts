148eaa1277bb92cc84a492a910b2bb3d6a7e8b68
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 12:59:36 +0200

    FIXED wrong logging

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
-  console.log('params ?', req.params['code'], req.params['scope']);
+  console.log('query ?', req.query['code'], req.query['scope']);
   res.send('Hello world !');
 });
 
