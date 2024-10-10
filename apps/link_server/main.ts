49c6f5cebe4a8367ad2da3d3f5a63a4c4baaee3d
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 23:33:45 +0200

    TRY to fix again

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -8,11 +8,8 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-});
-
-app.post('/aztro-link', (req, res) => {
-  const { code } = req.query;
   res.redirect(
+    307,
     `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
   );
 });
