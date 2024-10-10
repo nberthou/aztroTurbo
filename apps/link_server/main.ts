e21eb10a43179bcb40628358e4cba6cac4eeb883
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 23:29:10 +0200

    TRY to fix redirect

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -8,12 +8,13 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-  res.redirect('/aztro-link/token');
 });
 
 app.post('/aztro-link', (req, res) => {
   const { code } = req.query;
-  `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`;
+  res.redirect(
+    `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
+  );
 });
 
 app.get('/aztro-link/token', (req, res) => {
