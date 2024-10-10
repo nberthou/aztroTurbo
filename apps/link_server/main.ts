a156181b17aa346f0dc07c76b49d604582f22e29
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 17:38:22 +0200

    ADD post request ?

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -8,11 +8,10 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-  res
-    .status(301)
-    .redirect(
-      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
-    );
+});
+
+app.post('/aztro-link/token', (req, res) => {
+  `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`;
 });
 
 app.get('/aztro-link/token', (req, res) => {
