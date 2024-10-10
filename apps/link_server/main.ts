e94e3d596dfd66db29fead23de807d1422dd4db7
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 17:46:57 +0200

    FIXED no code query param

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -11,7 +11,8 @@ app.get('/aztro-link', (req, res) => {
   res.redirect('/aztro-link/token');
 });
 
-app.post('/aztro-link/token', (req, res) => {
+app.post('/aztro-link', (req, res) => {
+  const { code } = req.query;
   `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`;
 });
 
