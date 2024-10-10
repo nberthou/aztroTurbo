477096ccf59289b9a2ad74d0bb250e97151d9473
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 13:16:34 +0200

    ADD dotenv + CONTINUED twitch login flow

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
-  console.log('query ?', req.query['code'], req.query['scope']);
-  res.send('Hello world !');
+  const { code } = req.query;
+  res.redirect(
+    `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type="authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
+  );
+});
+
+app.get('/aztro-link/token', (req, res) => {
+  console.log('req.query', req.query);
 });
 
 app.listen(port, () => {
