92359ae6e63cc4fd1f4b20b2a38d7b602e3ecb9c
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 13:28:45 +0200

    ADD axios + TRY to use axios

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-  res.redirect(
+  axios.post(
     `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
   );
 });
