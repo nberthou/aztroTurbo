125e2f785c4238f3c5a129146ca01889dd63b263
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 13:40:49 +0200

    REMOVED axios

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import axios from 'axios';
 
 dotenv.config();
 
@@ -9,9 +8,11 @@ const port = 7176;
 
 app.get('/aztro-link', (req, res) => {
   const { code } = req.query;
-  axios.get(
-    `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
-  );
+  res
+    .status(301)
+    .redirect(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID ?? ''}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=https://api.azgold.fr/aztro-link/token`
+    );
 });
 
 app.get('/aztro-link/token', (req, res) => {
