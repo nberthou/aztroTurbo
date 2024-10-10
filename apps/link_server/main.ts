671ecc4c438cea5559ffefb39b8412ba5840a1f9
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-10 07:51:08 +0200

    ADD ! to test deploy

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -3,8 +3,8 @@ import express from 'express';
 const app = express();
 const port = 7176;
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.get('/aztro-link', (req, res) => {
+  res.send('Hello world !');
 });
 
 app.listen(port, () => {
