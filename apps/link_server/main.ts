c80f70ba2dc7168283014b762de6e79fb96ec9fd
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-10-14 07:49:34 +0200

    FIXED wrong id name

--- apps/link_server/main.ts
+++ apps/link_server/main.ts
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
               },
             })
             .then(async (response: any) => {
-              const twitchId = response.data.data[0].twitchId;
+              const twitchId = response.data.data[0].id;
               if (twitchId) {
                 await mergeDiscordAndTwitchUser(state as string, twitchId);
                 res.status(200).send('Vous pouvez fermer cette fenêtre désormais !');
